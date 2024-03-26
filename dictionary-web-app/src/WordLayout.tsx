64ea2743ddef7ac1b3e7eeef1ba0c79faf5c2771
Pietrell <piotrplotast2006@gmail.com>
2024-03-26 20:36:28 +0100

    added loading when data is being pulled from api

--- dictionary-web-app/src/WordLayout.tsx
+++ dictionary-web-app/src/WordLayout.tsx
@@ -13,13 +13,16 @@ export default function WordLayout() {
   };
   //get word from api
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchedWord}`)
       .then((response) => {
         setWord(response.data);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error(error);
+        setIsLoading(false);
       });
   }, [searchedWord]);
 
@@ -27,11 +30,23 @@ export default function WordLayout() {
   const spelling = word[0]?.phonetics[0]?.text;
   const audio = word[0]?.phonetics[0]?.audio;
 
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div>
       <SearchBar onProps={handlePropsFromSearch} />
-      <ActiveWord activeWord={activeWord} spelling={spelling} audio={audio} />
-      <WordExplanation word={word} />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <ActiveWord
+            activeWord={activeWord}
+            spelling={spelling}
+            audio={audio}
+          />
+          <WordExplanation word={word} />
+        </>
+      )}
     </div>
   );
 }
