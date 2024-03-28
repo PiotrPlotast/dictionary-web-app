9439a0682eec5d711241abff81e3f3f1576fc72c
Pietrell <piotrplotast2006@gmail.com>
2024-03-28 22:08:27 +0100

    cleaning code

--- dictionary-web-app/src/SearchBar.tsx
+++ dictionary-web-app/src/SearchBar.tsx
@@ -2,16 +2,13 @@ import { debounce } from "lodash";
 import SearchIcon from "./assets/images/icon-search.svg";
 export default function SearchBar({ onProps }) {
   const debouncedOnProps = debounce(onProps, 600);
-  const SendPropsToParent = () => {
-    props.onProps();
-  };
   return (
     <div className="bg-searchBarBgColor flex justify-between px-6 py-3 rounded-2xl">
       <input
         type="text"
         id="search-bar"
         placeholder="Search"
-        className="bg-searchBarBgColor text-[1rem] text-textColor font-bold"
+        className="bg-searchBarBgColor text-[1rem] text-textColor font-bold w-full outline-secondaryColor"
         onChange={(e) => debouncedOnProps(e.target.value)}
       />
       <img src={SearchIcon} alt="" />
