e99db0f578e1c6d4386d3636afa97a71452129cd
Pietrell <piotrplotast2006@gmail.com>
2024-03-28 22:40:21 +0100

    adding styles to the component

--- dictionary-web-app/src/WordExplanation.tsx
+++ dictionary-web-app/src/WordExplanation.tsx
@@ -1,3 +1,5 @@
+import NewWindowImg from "./assets/images/icon-new-window.svg";
+
 export default function WordExplanation({ word }) {
   return (
     <div>
@@ -11,30 +13,45 @@ export default function WordExplanation({ word }) {
               <div className="ml-4 h-[1px] w-full bg-decorationColor"></div>
             </div>
             <h3 className="text-accentColor">Meaning</h3>
-            <ul className="mt-4 list-disc px-4 marker:text-secondaryColor">
+            <ul className="mt-4 list-disc px-4 marker:text-secondaryColor flex flex-col gap-3">
               {meaning.definitions.map((definition, index) => {
                 return (
-                  <li key={index} className="text-bodyM">
-                    {definition.definition}
-                    {definition.example && (
-                      <p className="text-secondaryColor text-bodyM">
-                        <span className="font-bold">Example: </span>
-                        {definition.example}
-                      </p>
-                    )}
-                    {definition.synonyms && definition.synonyms.length > 0 && (
-                      <p className="text-secondaryColor text-bodyM">
-                        <span className="font-bold">Synonyms: </span>
-                        {definition.synonyms.join(", ")}
-                      </p>
-                    )}
-                  </li>
+                  <>
+                    <li key={index} className="text-[0.94rem]">
+                      <p>{definition.definition}</p>
+                      {definition.example && (
+                        <p className=" text-accentColor text-[0.94rem]">
+                          "{definition.example}"
+                        </p>
+                      )}
+                      {definition.synonyms &&
+                        definition.synonyms.length > 0 && (
+                          <p className="text-secondaryColor text-bodyM">
+                            <span className="font-bold">Synonyms: </span>
+                            {definition.synonyms}
+                          </p>
+                        )}
+                    </li>
+                  </>
                 );
               })}
             </ul>
           </div>
         );
       })}
+      <div className=" h-[1px] w-full bg-decorationColor mt-8 mb-6"></div>
+      <div className=" mb-20">
+        <h3 className="underline text-bodyS">Source</h3>
+        <a
+          className="underline cursor-pointer text-bodyS flex items-center gap-2"
+          href={word[0].sourceUrls[0]}
+        >
+          <span>{word[0].sourceUrls[0]}</span>
+          <span>
+            <img src={NewWindowImg} alt="" />
+          </span>
+        </a>
+      </div>
     </div>
   );
 }
