dc1f3799e010c68c88a2512c77bd586d0c033c4f
Pietrell <piotrplotast2006@gmail.com>
2024-03-31 13:21:39 +0200

    added darkmode colors

--- dictionary-web-app/src/ActiveWord.tsx
+++ dictionary-web-app/src/ActiveWord.tsx
@@ -16,7 +16,9 @@ export default function ActiveWord({
   return (
     <div className="flex justify-between items-center mt-6">
       <div className="flex flex-col gap-2">
-        <h1 className=" text-[2rem] font-bold text-textColor">{activeWord}</h1>
+        <h1 className=" text-[2rem] font-bold text-textColor dark:text-darkThemeTextColor">
+          {activeWord}
+        </h1>
         <h2 className=" text-secondaryColor text-bodyM">{spelling}</h2>
       </div>
       <button onClick={playWordSound} className="w-12 h-12">
