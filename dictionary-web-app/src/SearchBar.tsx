edcf72fb217f2e47494304b52ff3f00fcd8f979e
Pietrell <piotrplotast2006@gmail.com>
2024-03-31 13:20:54 +0200

    added darkmode styles

--- dictionary-web-app/src/SearchBar.tsx
+++ dictionary-web-app/src/SearchBar.tsx
@@ -3,12 +3,12 @@ import SearchIcon from "./assets/images/icon-search.svg";
 export default function SearchBar({ onProps }) {
   const debouncedOnProps = debounce(onProps, 600);
   return (
-    <div className="bg-searchBarBgColor flex justify-between px-6 py-3 rounded-2xl">
+    <div className="bg-searchBarBgColor dark:bg-darkThemeSearchBarBgColor flex justify-between px-6 py-3 rounded-2xl">
       <input
         type="text"
         id="search-bar"
         placeholder="Search"
-        className="bg-searchBarBgColor text-[1rem] text-textColor font-bold w-full outline-secondaryColor"
+        className="bg-searchBarBgColor dark:bg-darkThemeSearchBarBgColor text-[1rem] text-textColor dark:text-darkThemeTextColor font-bold w-full outline-secondaryColor"
         onChange={(e) => debouncedOnProps(e.target.value)}
       />
       <img src={SearchIcon} alt="" />
